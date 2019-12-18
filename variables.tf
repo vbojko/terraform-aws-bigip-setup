d5d847852234bdf8d60a5d5514f67bc22ee4a1ee
Daniel Schuler <daniel_schuler@me.com>
2019-12-18 16:45:35 +0100

    Set Default US Region

--- variables.tf
+++ variables.tf
@@ -8,37 +8,37 @@ variable "prefix" {
 ## Europe Regions need oder Jumphost and BigIP Instance Typs1
 ## Uncomment needed region below
 
-## US (Oregon)
-# variable "region" {
-#   default = "us-west-2"
-# }
-#
-# variable "azs" {
-#   default = ["us-west-2a", "us-west-2b"]
-# }
-#
-# variable "ec2_bigip_type" {
-#   default = "c4.xlarge"
-# }
-# variable "ec2_ubuntu_type" {
-#   default = "t2.xlarge"
-# }
-
-## Europe (Stockholm)
+# US (Oregon)
 variable "region" {
-  default = "eu-north-1"
+  default = "us-west-2"
 }
 
 variable "azs" {
-  default = ["eu-north-1a", "eu-north-1b"]
+  default = ["us-west-2a", "us-west-2b"]
 }
 
 variable "ec2_bigip_type" {
-  default = "c5.xlarge"
+  default = "c4.xlarge"
 }
 variable "ec2_ubuntu_type" {
-  default = "t3.xlarge"
+  default = "t2.xlarge"
 }
+
+## Europe (Stockholm)
+# variable "region" {
+#   default = "eu-north-1"
+# }
+#
+# variable "azs" {
+#   default = ["eu-north-1a", "eu-north-1b"]
+# }
+#
+# variable "ec2_bigip_type" {
+#   default = "c5.xlarge"
+# }
+# variable "ec2_ubuntu_type" {
+#   default = "t3.xlarge"
+# }
 
 variable "cidr" {
   default = "10.0.0.0/16"
